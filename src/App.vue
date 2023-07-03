ade4aa0d4940272d13e6d3f03644c75998e9653c
Krzysztof <olejnik.contact@gmail.com>
2023-07-03 19:05:27 +0200

    three main components, materialize css

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <script setup>
+import Navi from "./components/Navi.vue";
+import Content from "./components/Navi.vue";
+import Foot from "./components/Navi.vue";
 
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
 </script>
 
+<script>
+export default {
+  data() {
+    return {
+      toDoList: [
+        "Laundry",
+        "Shopping",
+        "Take out the garbage",
+        "Interview exercise",
+        "Squaring the circle",
+        "Doubling the cube",
+        "Angle trisection"
+      ]
+    }
+  }
+}
+</script>
+
+
 <template>
-  Hello World
+  <Navi />
+  <Content />
+  <Foot />
 </template>
 
 <style scoped>
