95a0e04885edcf4e5c27ba98d8100a01bf90beb1
Krzysztof <olejnik.contact@gmail.com>
2023-07-04 10:15:32 +0200

    addForm

--- src/components/AddForm.vue
+++ src/components/AddForm.vue
@@ -2,10 +2,72 @@
 
 </script>
 
-<template>
+<script>
+export default {
+  data() {
+    return {
+      task: {
+        title: '',
+        content: '',
+        status: ''
+      }
+    }
+  },
+  mounted() {
+    M.FormSelect.init(this.$refs.select);
+  },
+  methods: {
+    submit: function() {
+      this.$refs.form.reset();
+      //document.querySelectorAll("form").forEach(form => form.reset());
+    }
+  }
+}
+</script>
 
+<template>
+  <div class="row">
+    <div class="form-container col s12">
+      <h5 class="title">Add new task</h5>
+      <form ref="form" @submit.prevent="submit">
+        <div class="input-field">
+          <input id="title" type="text" class="validate">
+          <label for="title">Title</label>
+        </div>
+        <div class="input-field">
+          <textarea id="content" class="materialize-textarea"></textarea>
+          <label for="content">Content</label>
+        </div>
+        <div class="input-field">
+          <select ref="select">
+            <option value="new">New</option>
+            <option value="inProgress">In progress</option>
+            <option value="completed">Completed</option>
+            <option value="onHold">On Hold</option>
+            <option value="abandoned">Abandoned</option>
+          </select>
+          <label>Select status</label>
+        </div>
+        <div class="container">
+          <div class="row center-align">
+            <button class="btn waves-effect waves-light center">
+              Submit
+              <i class="material-icons right">send</i>
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
+.title {
+  margin-bottom: 30px;
+}
+.form-container {
+  border: 2px solid grey;
+  border-radius: 5px;
+  margin-top: 7.5px;
+}
 </style>
